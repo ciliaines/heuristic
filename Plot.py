f0daa786c04a2b740bb924ab8ffe30acdedd4576
ciliaines <ciliaines2@gmail.com>
2024-09-10 19:01:02 +0200

    arrglar erratas y mejoras en el dibujo

--- Plot.py
+++ Plot.py
@@ -18,14 +18,12 @@ def network_topology(Sources, Destinations):
     # Build the graph
     G=nx.from_pandas_edgelist(df, 'from', 'to')
     # Plot the graph
-
     plot_network= plt.figure(1,figsize=(28, 14))
-   
     #plt.subplot(221)
     plt.subplot2grid((4,2),(0,0))
     plt.axis('off')
-    plt.title("Network  Topology", fontsize=10)
-    nx.draw(G, with_labels=True,  node_size=400, font_size=10, edge_color='gray', width=1.0)   
+    plt.title("Network  Topology", fontsize=12)
+    nx.draw(G, with_labels=True,  node_size=400, font_size=15, edge_color='gray', width=1.0)   
 
     
 
@@ -55,13 +53,13 @@ def gantt_chart_generator(Result_offsets, Repetitions, Streams_Period):
     plt.subplot2grid((4,2),(1,0), colspan=2, rowspan=3)
     #plt.figure(figsize=(12, 5))
     plt.barh(y=df.Process_Name, left=df.Start, width=12, color=df.Color)
-    plt.grid(axis='x', alpha=0.5)
-    plt.grid(axis='y',alpha=0.5)
+    plt.grid(axis='x', alpha=1)
+    plt.grid(axis='y',alpha=1)
     plt.ylabel("Frames")
     plt.xlabel("Time in microseconds")
-    plt.xticks(fontsize=7)
-    plt.yticks(fontsize=7)
-    plt.title("Gantt Chart", fontsize=10)
+    plt.xticks(fontsize=5)
+    plt.yticks(fontsize=5)
+    plt.title("Gantt Chart", fontsize=15)
     plt.tight_layout()
     #plt.savefig('testing.png')
     #plt.show() 
@@ -71,14 +69,14 @@ def gantt_chart_generator(Result_offsets, Repetitions, Streams_Period):
 def information_generator(Num_of_Frames, Streams_Period, Link_order_Descriptor, Network_links, Streams_links_paths,input_name):
     #plt.subplot(222)
     plt.subplot2grid((4,2),(0,1))
-    plt.text(0.1, 0.9, "Network links: " + str(Network_links), bbox=dict(facecolor='red', alpha=0.5), fontsize=10)
-    plt.text(0.1, 0.75, "Frames per stream: " + str(Num_of_Frames), bbox=dict(facecolor='red', alpha=0.5), fontsize=10)
-    plt.text(0.1, 0.6, "Stream periods: " + str(Streams_Period), bbox=dict(facecolor='red', alpha=0.5), fontsize=10)
-    plt.text(0.1, 0.45, "Indexed link order per stream:  " + str(Link_order_Descriptor), bbox=dict(facecolor='red', alpha=0.5), fontsize=10)
+    plt.text(0.1, 0.9, "Network links: " + str(Network_links), bbox=dict(facecolor='red', alpha=0.6), fontsize=20)
+    plt.text(0.1, 0.7, "Frames per stream: " + str(Num_of_Frames), bbox=dict(facecolor='red', alpha=0.6), fontsize=20)
+    plt.text(0.1, 0.5, "Stream periods: " + str(Streams_Period), bbox=dict(facecolor='red', alpha=0.6), fontsize=20)
+    plt.text(0.1, 0.3, "Indexed link order per stream:  " + str(Link_order_Descriptor), bbox=dict(facecolor='red', alpha=0.6), fontsize=20)
     wrapped_text = "\n".join(textwrap.wrap(str(Streams_links_paths), width=120))
-    plt.text(0.1, 0.25, "Stream paths:  " + str(wrapped_text), bbox=dict(facecolor='red', alpha=0.5), fontsize=10)
+    plt.text(0.1, 0.1, "Stream paths:  " + str(wrapped_text), bbox=dict(facecolor='red', alpha=0.6), fontsize=20)
     #plt.text(0.1, 0.1, "Stream paths: \n " + str(Streams_links_paths), bbox=dict(facecolor='red', alpha=0.5), fontsize=10)
-    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
+    #plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
     plt.axis('off')
     name="Solutions/"+input_name+".png"
     plt.savefig(name, bbox_inches='tight', pad_inches=0)
